ea4da7385dad61b7c77c022cd7821aec61a14142
Marwane Ouahid <ouahidmarwanee@gmail.com>
2025-11-26 09:18:50 +0100

    Initial commit

---
+++ TP_SPRING_MVC_Thymleaf1-master/src/test/java/ma/mundia/Thymeleaf/marwane/ouahid/ThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.mundia.Thymeleaf.marwane.ouahid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
